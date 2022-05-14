a5915fce5e7daf4fbe5f927231173790908ee97d
Enzo <EnzoPiccolo7>
2022-05-14 21:25:46 +0200

    primo commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+ let uno = 1;
+let due = 2;
+let tre = 3;
+let quattro = 4;
+let cinque = 5;
+let sei = 6;
+let sette = 7;
+let otto = 8;
+let nove = 9;
+let zero = 0;
+let reset;
+let addizione = '+';
+let meno = '-';
+let moltiplicazione = '*';
+let divisione = '/';
+let punto = '.';
+function nov() {
+document.getElementById('risultato').innerHTML = nove;    
+}
+function ott() {
+    document.getElementById('risultato').innerHTML = otto;    
+}
+function sett() {
+    document.getElementById('risultato').innerHTML = sette;    
+}
+function se() {
+    document.getElementById('risultato').innerHTML = sei;    
+}
+function cinqu() {
+    document.getElementById('risultato').innerHTML = cinque;    
+}   
+function quattr() {
+    document.getElementById('risultato').innerHTML = quattro;    
+}
+function tr() {
+    document.getElementById('risultato').innerHTML = tre;    
+}                                                                                   
+function du() {
+    document.getElementById('risultato').innerHTML = due;    
+}
+function un() {
+    document.getElementById('risultato').innerHTML = uno;    
+}
+function zer() {
+    document.getElementById('risultato').innerHTML = zero;    
+}
+function addizion() {
+    document.getElementById('risultato').innerHTML = addizione;    
+}
+function men() {
+    document.getElementById('risultato').innerHTML = sottrazione;    
+}
+function molti() {
+    document.getElementById('risultato').innerHTML = moltiplicazione;    
+}
+function divizion() {
+    document.getElementById('risultato').innerHTML = divisione;    
+}
+function rese() {
+    document.getElementById('risultato').innerHTML = reset;    
+} 
+function totale() {
+    document.getElementById('risultato').innerHTML = totale;    
+}
+function punt() {
+    document.getElementById('risultato').innerHTML = punto;    
+}
+
